a6b6fb96f15d2cc9e1d1e886241d62e79e3057fc
Eduardo Tachotte <bfwaend@gmail.com>
2025-02-24 20:49:37 -0300

    Finish ex02

--- python-0/ex02/find_ft_type.py
+++ python-0/ex02/find_ft_type.py
@@ -4,7 +4,7 @@ def all_thing_is_obj(object: any) -> int:
         return 42
 
     objType = object.__class__
-    typeName = objType.__name__.capitalize() if not isinstance(object, str) else str
+    typeName = objType.__name__.capitalize() if not isinstance(object, str) else object
     funnyStr = " is in the kitchen" if isinstance(object, str) else ""
     print(f'{typeName}{funnyStr} : {objType}')
     return 42
