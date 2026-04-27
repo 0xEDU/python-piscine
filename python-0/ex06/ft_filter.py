93e5a41aa84f6e4b835ce5afe38788b8ec81186b
Eduardo Tachotte <bfwaend@gmail.com>
2026-04-27 18:22:10 -0300

    Fix filter __doc__

--- python-0/ex06/ft_filter.py
+++ python-0/ex06/ft_filter.py
@@ -1,8 +1,4 @@
 def ft_filter(function_to_apply, list_of_inputs):
-    """
-ft_filter(function or None, iterable) --> filter object
-
-Return an iterator yielding those items of iterable for which function(item)
-is true. If function is None, return the items that are true.
-    """
+    """Return an iterator yielding those items of iterable for which function(item)
+    is true. If function is None, return the items that are true."""  # noqa: E501
     return [x for x in list_of_inputs if function_to_apply(x)]
